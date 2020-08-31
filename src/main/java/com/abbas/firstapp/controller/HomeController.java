88fbf59640106fec57ebd69d6c26ab06d2973929
MAbbasAlvi <abbas_alvi512@hotmail.com>
2020-08-31 22:28:37 +0500

    My Commit

---
+++ src/main/java/com/abbas/firstapp/controller/HomeController.java
@@ -0,0 +1,27 @@
+package com.abbas.firstapp.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String sayHelloGet(){
+        return "Hello World from Get!";
+    }
+
+    @PostMapping("/")
+    public String sayHelloPost(){
+        return "Hello World from Post!";
+    }
+
+    @PutMapping("/")
+    public String sayHelloPut(){
+        return "Hello World from Put!";
+    }
+
+    @DeleteMapping("/")
+    public String sayHelloDelete(){
+        return "Hello World from Delete!";
+    }
+}
